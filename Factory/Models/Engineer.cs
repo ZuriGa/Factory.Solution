a02f4d430ce89278b16676c1d1f898a00db84d76
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-09 10:59:58 -0800

    add Engineer controller + model + view folders

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public DateTime HireDate { get; set; } = DateTime.Now;
+
+  }
+}
