5bd261fe19702d3a2cd4f95ffbf36cf13d9d4594
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-09 14:51:52 -0800

    add machine model + controller + view

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -6,6 +6,8 @@ namespace Factory.Models
   {
     public DbSet<Engineer> Engineers { get; set; }
 
+    public DbSet<Machine> Machines { get; set; }
+
     public FactoryContext(DbContextOptions options) : base (options) { }
   }
 }
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+
+    public string MachineName { get; set; }
+
+    public int Installed { get; set; }
+  }
+}
