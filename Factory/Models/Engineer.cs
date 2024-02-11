48a917745314dfa557dd0b867036a432363127ae
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-11 08:41:06 -0800

    debug on homecontroller + updated splashpage

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace Factory.Models
@@ -6,6 +7,7 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "Engineer name is required")]
     public string EngineerName { get; set; }
     public DateTime HireDate { get; set; } = DateTime.Now;
 
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace Factory.Models
@@ -6,9 +7,9 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-
+    [Required(ErrorMessage = "Machine name is required")]
     public string MachineName { get; set; }
-
+    [Required(ErrorMessage = "Installation date is required")]
     public string Installed { get; set; }
 
     public List<MachineEngineer> JoinEntities { get; set; }
